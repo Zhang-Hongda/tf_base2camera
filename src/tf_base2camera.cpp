8f556ef5e2779706a16e5cd338e77eb2879df8de
Zhang-Hongda <2210010444@qq.com>
2019-05-14 20:17:24 +0800

    format code

--- src/tf_base2camera.cpp
+++ src/tf_base2camera.cpp
@@ -1,7 +1,8 @@
 #include <ros/ros.h>
 #include <tf/transform_broadcaster.h>
 
-int main(int argc, char** argv){
+int main(int argc, char** argv)
+{
   ros::init(argc, argv, "camera_to_base");
   ros::NodeHandle node("~");
 
@@ -9,13 +10,14 @@ int main(int argc, char** argv){
   tf::Transform transform;
 
   ros::Rate rate(10.0);
-  while (node.ok()){
+  while (node.ok())
+  {
     std::vector<double> position;
     std::vector<double> orientation;
-    node.getParam("position",position);
-    node.getParam("orientation",orientation);
-    transform.setOrigin( tf::Vector3( position[0],position[1],position[2]));
-    transform.setRotation( tf::Quaternion(orientation[0],orientation[1],orientation[2],orientation[3]) );
+    node.getParam("position", position);
+    node.getParam("orientation", orientation);
+    transform.setOrigin(tf::Vector3(position[0], position[1], position[2]));
+    transform.setRotation(tf::Quaternion(orientation[0], orientation[1], orientation[2], orientation[3]));
     br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "kinect2_link"));
     rate.sleep();
   }
